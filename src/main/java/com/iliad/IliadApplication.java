6e62d616a19d32782e0f0eae884424fd1a2cdfdb
Patrick K <pmkelebe@yahoo.co.uk>
2021-03-08 12:03:50 +0000

    Added OpenAPI support

--- src/main/java/com/iliad/IliadApplication.java
+++ src/main/java/com/iliad/IliadApplication.java
@@ -1,9 +1,12 @@
 package com.iliad;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info=@Info(title = "Iliad Api",description = "parsing numbers list",version="1.0") )
 public class IliadApplication {
 
 	public static void main(String[] args) {
